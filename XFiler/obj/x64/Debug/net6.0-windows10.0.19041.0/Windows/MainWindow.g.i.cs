eba3bdd4b054298e37e8fbfc2a0dfa9a94284e7b
bkmzli <bkmzlitel@gmail.com>
2022-12-10 15:02:36 +0500

    Debag?

--- XFiler/obj/x64/Debug/net6.0-windows10.0.19041.0/Windows/MainWindow.g.i.cs
+++ XFiler/obj/x64/Debug/net6.0-windows10.0.19041.0/Windows/MainWindow.g.i.cs
@@ -72,7 +72,7 @@ namespace XFiler {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/XFiler;component/windows/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/XFiler;V0.2.10.0;component/windows/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\..\Windows\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
